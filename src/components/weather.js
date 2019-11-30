8cab8114ccecd9c4402726eb1aa228aa51ca4f26
Raseshwari <32209693+Raseshwari@users.noreply.github.com>
2019-11-30 03:55:45 -0500

    fetching list of 5 days forecast

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,10 +5,11 @@ const Api_Key = "b7cc7d16f6a2279175df66665d9e2aef";
 class Weather extends React.Component{
     constructor(props){
         super(props);
+
     }
 
     async componentDidUpdate(){
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=${Api_Key}`);
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.props.city}&appid=${Api_Key}&cnt=5`);
         const response = await api_call.json();
         console.log(response);
     }
